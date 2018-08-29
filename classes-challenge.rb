24caab2d4a0b9cfbb31b33efe9627358f31e1f9f
Allan Gao <allangao95@gmail.com>
2018-08-29 16:23:08 +1000

    add objects challenges

---
+++ classes-challenge.rb
@@ -0,0 +1,41 @@
+# class Cat
+#     def initialize(name)
+#         @name = name #specialised method/default    
+#     end
+
+#     def speak
+#         puts "#{@name} says meow"
+#     end
+# end
+
+# newCat= Cat.new("ginger")
+# newCat.speak
+
+
+
+
+class Dog
+    attr_accessor :location 
+    def initialize(name, age, location)
+        @name = name
+        @age = age
+        @location = location
+        @walks = 0
+        # @walks = []
+    end
+
+    def walks
+        @walks += 1
+        puts "I have been on #{@walks} walks"
+    end
+
+    def speak
+        puts "woof, my name is #{@name}"
+        puts "I am #{@age.to_i} years old"
+        puts "I live in #{@location}"
+    end
+end
+
+doggo = Dog.new("Ralph", 12, "Brisbane")   
+doggo.walks
+doggo.walks
